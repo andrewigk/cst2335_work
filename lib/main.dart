5236ba37ab0bf9b8515696ed274cba2499b3f3e2
Andrew Kim <andrew.igk@gmail.com>
2024-07-03 14:41:48 -0400

    Lab 6

--- lib/main.dart
+++ lib/main.dart
@@ -34,6 +34,7 @@ class MyHomePage extends StatefulWidget {
 
   final String title;
 
+
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
@@ -45,28 +46,10 @@ class _MyHomePageState extends State<MyHomePage> {
     super.initState();
     _controller = TextEditingController();
     _controller2 = TextEditingController();
-    loadData();
 
   }
 
-  void loadData() async{
-    EncryptedSharedPreferences prefs = EncryptedSharedPreferences();
-    var login = await prefs.getString("Username");
-    var passWord = await prefs.getString("Password");
-    _controller.text = login;
-    _controller2.text = passWord;
-    if(login != "" || passWord != "") {
-      final snackBar = SnackBar(content: Text('Your previously used Username and Login have been loaded.'),
-          action: SnackBarAction(label: 'Clear Saved Data', onPressed: () {
-            EncryptedSharedPreferences prefs = EncryptedSharedPreferences();
-            prefs.remove("Username");
-            prefs.remove("Password");
-            _controller.text = "";
-            _controller2.text = "";
-          },));
-      ScaffoldMessenger.of(context).showSnackBar(snackBar);
-    }
-    }
+
 
 
   @override
@@ -76,10 +59,9 @@ class _MyHomePageState extends State<MyHomePage> {
     super.dispose();
   }
 
-  var isChecked = false;
-  var imageSource = "images/question.png";
   late TextEditingController _controller;
   late TextEditingController _controller2;
+  List<String> words =  [] ;
 
   @override
   Widget build(BuildContext context) {
@@ -92,64 +74,53 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            TextField(controller: _controller, decoration: InputDecoration(
-                hintText: "Username", border: OutlineInputBorder(), labelText: "Log-In"
-            )), SizedBox(height: 10),
-            TextField(controller :_controller2, decoration: InputDecoration(
-                hintText: "Password", border: OutlineInputBorder(), labelText: "Password"
-            ), obscureText:true),
-            ElevatedButton( onPressed: () {
-              showDialog<String>(
-                  context: context,
-                  builder: (BuildContext context) => AlertDialog(
-                      title: const Text('Save credentials?'),
-                      content: const Text('Click "Yes" to save Username/Password.'),
-                      actions: <Widget>[
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          children: <Widget>[
-                            OutlinedButton(onPressed: () {
-                              var userName = _controller.value.text;
-                              EncryptedSharedPreferences prefs = EncryptedSharedPreferences();
-                              prefs.setString("Username", userName);
-                              var password = _controller2.value.text;
-                              prefs.setString("Password", password);
-                              Navigator.pop(context);
-                            },
-                                child: Text("Yes")),
-                            OutlinedButton(onPressed: () {
-                              EncryptedSharedPreferences prefs = EncryptedSharedPreferences();
-                              prefs.remove("Username");
-                              prefs.remove("Password");
-                              Navigator.pop(context);
-                            },
-                                child: Text("No"))
+        child:
+        Column( mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children:[
+        Padding(padding: EdgeInsets.fromLTRB(16, 0, 16, 0),
+        child: Row( mainAxisAlignment: MainAxisAlignment.spaceBetween, children:[
+          Expanded(
+              child:TextField(controller: _controller,
+            decoration: InputDecoration(labelText: "Enter a to-do item..."))
+          ),
+          ElevatedButton( child:Text("Add item"), onPressed:() {
+            setState(() {
+              words.add( _controller.value.text );
+              _controller.text = "";
+            });
+          }),
+        ]),
+      ),
 
-                          ]
-                        )
+        Expanded(child: Padding(padding: EdgeInsets.fromLTRB(16, 24, 16, 0),
+            child:
+        ListView.builder(
+            itemCount: words.length,
+            itemBuilder: (context, rowNum) {
+              return Padding(padding: EdgeInsets.fromLTRB(16, 24, 16, 0),
+              child: Container(
+                      padding: const EdgeInsets.all(10.0),
+                      color: Colors.deepPurple[100],
+                      child:
+                  Row( mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Text("Task Number: $rowNum"),
+                    GestureDetector(
+                        child:  Text(words[rowNum]),
+                        onLongPress: () {
+                          final snackBar = SnackBar(content: Text("You tapped me ohohoho"));
+                          ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                        }
+                    ),
 
-                      ]));
-              var pword = _controller2.value.text;
-              setState(() {
-                if (pword  == "QWERTY123"){
-                  imageSource = "images/lightbulb.png";
-                  Navigator.pushNamed(context, '/profilePage');
-                }
-                else {
-                  imageSource = "images/stop.png";
-                }
-              });
-            }, child:  Text("Log In"),),
-            Image.asset(imageSource, width:300, height:300),
+                  ])));
+            }
+            ))
+        )]
 
-          ],
+
+
         ),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+    ));
   }
 }
